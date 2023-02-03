5d008bdfc110aeb57b15204c709198652b331e3b
Türcan <73202042+Turcan-vs@users.noreply.github.com>
2023-02-03 14:27:47 +0300

    .

--- client/src/Pages/Home/home.jsx
+++ client/src/Pages/Home/home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
 
+import Slider from '../../Component/Slider/slider'
+
 import './home.scss'
+
+
+
+
 
 const Home = () => {
   return (
-    <div className='home'>
-    Home
+
+    <div className="home">
+      <Slider/>
+      <div className="new-feature">
+        yeni halılar
+      </div>
+      <div className="customer-review">
+        müşteri yorumları
+      </div>
     </div>
   )
 }
